88a8fcad8210917209bd7b3451de23ba9c293c47
Connor Engel <cengel@lcs.on.ca>
2025-05-05 12:02:56 -0400

    added a  basic function for filtering

---
+++ SpellingMobile/Functions/Filtering.swift
@@ -0,0 +1,24 @@
+//
+//  Filtering.swift
+//  SpellingMobile
+//
+//  Created by Connor Engel on 2025-05-05.
+//
+
+// Define the function
+
+// name: filtering
+
+//inputs (parameters) :
+//  the origional array (originonallist)
+//  what to filter on (desiredOutcome)
+
+// outpu (return type) : an array filtered based on outcome
+
+func filtering(
+origionalList: [Result],
+on desiredOutcome: Outcome
+) -> [Outcome] {
+    // return an empty array
+    return []
+}
